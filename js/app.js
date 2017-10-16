2db23f8e206edc0be2da1faf9060afc1ecd3a943
chansung park <deep.diver.csp@gmail.com>
2017-10-17 01:00:39 +0900

    adding motion to the player when pressing keys - still collision check remains as well as goal touch.
    randomly spawn enemies
    - still randomly chosen speed generation remains.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const blockHeight = 83;
+const blockWidth = 101;
+const blockHeightQuarter = 83/4;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.speed = 0;
+
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // console.log(dt);
+    this.x++;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +22,65 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var allEnemies = [];
+
+myVar = setInterval(function() {
+    const row = Math.floor(Math.random() * 3) + 1;
+    const yLocation = blockHeight * row - blockHeightQuarter;
+
+    let newEnemy = new Enemy(0, yLocation);
+
+    allEnemies.push(newEnemy);
+}, 3000);
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y, character) {
+    this.x = x;
+    this.y = y;
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    this.sprite = 'images/' + character + '.png';
+};
+
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    if (key == 'up') {
+        if (this.y - blockHeight >= -blockHeightQuarter) {
+            this.y -= blockHeight;
+        }
+    }
+    else if (key == 'down') {
+        if (this.y + blockHeight <= blockHeight*5 -blockHeightQuarter) {
+            this.y += blockHeight;
+        }
+    }
+    else if (key == 'left') {
+        if (this.x - blockWidth >= 0) {
+            this.x -= blockWidth;
+        }
+    }
+    else if (key == 'right') {
+        if (this.x + blockWidth <= blockWidth*4) {
+            this.x += blockWidth;
+        }
+    }
+
+    console.log('x : ' + this.x + ", y : " + this.y);
+};
+
+var player = new Player(0, blockHeight * 5 - blockHeightQuarter,'char-boy');
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
